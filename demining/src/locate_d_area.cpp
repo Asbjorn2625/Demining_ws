db9220c7aaf4038ac516e4a1860f93e3aad11dd2
Miki0110 <Mikipregaard@yahoo.dk>
2020-11-18 14:07:13 +0100

    idk what is going on

--- demining/src/locate_d_area.cpp
+++ demining/src/locate_d_area.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <ros/ros.h>
-#include<geometry_msgs/Twist.h>
-#include<kobuki_msgs/BumperEvent.h>
-#include<kobuki_msgs/Sound.h>
+#include <geometry_msgs/Twist.h>
+#include <kobuki_msgs/BumperEvent.h>
+#include <kobuki_msgs/Sound.h>
+#include <tf/transform_listener.h>
 
 kobuki_msgs::BumperEvent bumpMsg;
 kobuki_msgs::Sound soundMsg;
 ros::Publisher sound_pub;
 ros::Subscriber bumperSub;
-//kobuki_msgs::Sound soundPub;
+tf::TransformListener listener;
+tf::StampedTransform transform;
 
-
-void callbackBump(const kobuki_msgs::BumperEvent &bumpMsg){
+void bumperHit(const kobuki_msgs::BumperEvent &bumpMsg){
     if(bumpMsg.state==1){
         soundMsg.value = 4;
         sound_pub.publish(soundMsg);
-        std::cout<<"\n fy faaaaaeeen \n";
         //skal slukke for lyden igen ellers køre den nogen gange i loop
         soundMsg.value = 1;
         sound_pub.publish(soundMsg);
+        std::cout << "y = " << transform.getOrigin().y() << "\n x = " << transform.getOrigin().x() << "\n";
     }
 
 }
@@ -28,8 +29,12 @@ int main(int argc, char *argv[])
    ros::init(argc,argv,"bumper");
     srand(time(NULL));
     ros::NodeHandle n;
-    sound_pub=n.advertise<kobuki_msgs::Sound > ("/mobile_base/commands/sound",-1);
-    bumperSub=n.subscribe("/mobile_base/events/bumper",1,callbackBump) ;
+    sound_pub=n.advertise<kobuki_msgs::Sound > ("/mobile_base/commands/sound", 2);
+
+   // listener.waitForTransform("/move_base/local_costmap/global_frame", "/move_base/local_costmap/robot_base_frame", ros::Time::now(), ros::Duration(2.0));
+   // listener.lookupTransform("/move_base/local_costmap/global_frame", "/move_base/local_costmap/robot_base_frame", ros::Time::now(), transform);
+
+    bumperSub=n.subscribe("/mobile_base/events/bumper",1,bumperHit);
 while(ros::ok())
 {
   
