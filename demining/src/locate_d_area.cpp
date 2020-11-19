7b08d34b6c267ec8cf23c8fe6b923c748ea3042c
Miki0110 <Mikipregaard@yahoo.dk>
2020-11-19 09:34:50 +0100

    on bumper hit = pose print

--- demining/src/locate_d_area.cpp
+++ demining/src/locate_d_area.cpp
@@ -3,14 +3,39 @@
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <kobuki_msgs/Sound.h>
+#include "nav_msgs/Odometry.h"
+#include "tf/tf.h"
 #include <tf/transform_listener.h>
+
 
 kobuki_msgs::BumperEvent bumpMsg;
 kobuki_msgs::Sound soundMsg;
 ros::Publisher sound_pub;
 ros::Subscriber bumperSub;
-tf::TransformListener listener;
-tf::StampedTransform transform;
+ros::Subscriber pose_subscriber;
+nav_msgs::Odometry turtlebot_odom_pose;
+
+void poseCallback(const nav_msgs::Odometry::ConstPtr & pose_message){
+	turtlebot_odom_pose.pose.pose.position.x=pose_message->pose.pose.position.x;
+	turtlebot_odom_pose.pose.pose.position.y=pose_message->pose.pose.position.y;
+	turtlebot_odom_pose.pose.pose.position.z=pose_message->pose.pose.position.z;
+
+	turtlebot_odom_pose.pose.pose.orientation.w=pose_message->pose.pose.orientation.w;
+	turtlebot_odom_pose.pose.pose.orientation.x=pose_message->pose.pose.orientation.x;
+	turtlebot_odom_pose.pose.pose.orientation.y=pose_message->pose.pose.orientation.y;
+	turtlebot_odom_pose.pose.pose.orientation.z=pose_message->pose.pose.orientation.z;
+}
+
+/* makes conversion from radian to degree */
+double radian2degree(double radianAngle){
+	return (radianAngle*57.2957795);
+}
+
+
+/* makes conversion from degree to radian */
+double degree2radian(double degreeAngle){
+	return (degreeAngle/57.2957795);
+}
 
 void bumperHit(const kobuki_msgs::BumperEvent &bumpMsg){
     if(bumpMsg.state==1){
@@ -19,28 +44,33 @@ void bumperHit(const kobuki_msgs::BumperEvent &bumpMsg){
         //skal slukke for lyden igen ellers køre den nogen gange i loop
         soundMsg.value = 1;
         sound_pub.publish(soundMsg);
-        std::cout << "y = " << transform.getOrigin().y() << "\n x = " << transform.getOrigin().x() << "\n";
+
+            ROS_INFO("robot current pose: (x = %.2f, y = %.2f, angle = %.2f)\n",
+                                                        turtlebot_odom_pose.pose.pose.position.x,
+										                turtlebot_odom_pose.pose.pose.position.y,
+										                radian2degree(tf::getYaw(turtlebot_odom_pose.pose.pose.orientation)));
+    
     }
 
 }
 
 int main(int argc, char *argv[])
 {
-   ros::init(argc,argv,"bumper");
+    ros::init(argc,argv,"locate_d_area");
     srand(time(NULL));
     ros::NodeHandle n;
     sound_pub=n.advertise<kobuki_msgs::Sound > ("/mobile_base/commands/sound", 2);
-
-   // listener.waitForTransform("/move_base/local_costmap/global_frame", "/move_base/local_costmap/robot_base_frame", ros::Time::now(), ros::Duration(2.0));
-   // listener.lookupTransform("/move_base/local_costmap/global_frame", "/move_base/local_costmap/robot_base_frame", ros::Time::now(), transform);
-
+    pose_subscriber = n.subscribe("/odom", 10, poseCallback);
     bumperSub=n.subscribe("/mobile_base/events/bumper",1,bumperHit);
+    
 while(ros::ok())
 {
   
     ros::spinOnce();
+
 }
 
 
     return 0;
 }
+
