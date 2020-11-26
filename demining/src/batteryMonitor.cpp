3f812988b7f74e0f1496fbc4dd4d0f5885455ae3
SimonHaaningAAU <san20@student.aau.dk>
2020-11-26 11:14:43 +0100

    Movement to origin coordinate followed by docking tested with coordinatePublisher

--- demining/src/batteryMonitor.cpp
+++ demining/src/batteryMonitor.cpp
@@ -1,60 +1,108 @@
 #include <ros/ros.h>
 
-#include <iostream>                       //iostream present for testing and error messages
+#include <iostream> //iostream present for testing and error messages
 
-#include <geometry_msgs/Pose2D.h>         //msgs for start pos subscriber
+#include <geometry_msgs/Pose2D.h> //msgs for start pos subscriber
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/simple_client_goal_state.h>
 #include <nav_msgs/Odometry.h>
 
-#include <kobuki_msgs/PowerSystemEvent.h> //Kobuki_node capable of detecting changes to the Power system http://docs.ros.org/en/api/kobuki_msgs/html/msg/PowerSystemEvent.html
-#include <sensor_msgs/BatteryState.h>     //Used for laptop battery information (could be used for Kobuki as well but havent figured out how yet)
-#include <kobuki_msgs/AutoDockingAction.h>//Used for the autodocking feature
-#include <kobuki_msgs/AutoDockingGoal.h>  //Used for the autodocking feature
+#include <kobuki_msgs/PowerSystemEvent.h>  //Kobuki_node capable of detecting changes to the Power system http://docs.ros.org/en/api/kobuki_msgs/html/msg/PowerSystemEvent.html
+#include <sensor_msgs/BatteryState.h>      //Used for laptop battery information (could be used for Kobuki as well but havent figured out how yet)
+#include <kobuki_msgs/AutoDockingAction.h> //Used for the autodocking feature
+#include <kobuki_msgs/AutoDockingGoal.h>   //Used for the autodocking feature
 
 //Following inclusions used for MovingToPosition class
-#include <visualization_msgs/Marker.h>
-#include <visualization_msgs/MarkerArray.h>
 #include <tf/transform_listener.h>
 #include <geometry_msgs/PointStamped.h>
 
-class MovingToPosition{
-  private:
-
+class MovingToPosition
+{
+private:
   tf::TransformListener listener;
 
   typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-  public:
 
-  double getPosition(double mapPose[]){
-  ros::spinOnce();
-  geometry_msgs::PoseStamped pBase, pMap;
-  pBase.header.frame_id = "base_link";
-  pBase.pose.position.x = 0.0;
-  pBase.pose.position.y = 0.0;
-  pBase.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
-  ros::Time current_transform = ros::Time::now();
-  ros::Duration(0.2).sleep();
-  listener.getLatestCommonTime(pBase.header.frame_id, "map", current_transform, NULL);
-  ros::Duration(0.2).sleep();
-  pBase.header.stamp = current_transform;
-  listener.transformPose("map", pBase, pMap);
-  //printf("robot pose: (%.2f, %.2f)\n", pMap.pose.position.x, pMap.pose.position.y);
-  mapPose[0] = pMap.pose.position.x;
-  mapPose[1] = pMap.pose.position.y;
+public:
+  double getPosition(double mapPose[])
+  {
+    ros::spinOnce();
+    geometry_msgs::PoseStamped pBase, pMap;
+    pBase.header.frame_id = "base_link";
+    pBase.pose.position.x = 0.0;
+    pBase.pose.position.y = 0.0;
+    pBase.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
+    ros::Time current_transform = ros::Time::now();
+    ros::Duration(0.2).sleep();
+    listener.getLatestCommonTime(pBase.header.frame_id, "map", current_transform, NULL);
+    ros::Duration(0.2).sleep();
+    pBase.header.stamp = current_transform;
+    listener.transformPose("map", pBase, pMap);
+    //printf("robot pose: (%.2f, %.2f)\n", pMap.pose.position.x, pMap.pose.position.y);
+    mapPose[0] = pMap.pose.position.x;
+    mapPose[1] = pMap.pose.position.y;
   }
 
-  void moveToMap(double posX, double posY, const char* oriantation){
-  MoveBaseClient ac("move_base", true);
+  void moveTo(double posX, double posY, const char *oriantation)
+  {
+    MoveBaseClient ac("move_base", true);
 
     //wait for the action server to come up
-    while(!ac.waitForServer(ros::Duration(5.0))){
+    while (!ac.waitForServer(ros::Duration(5.0)))
+    {
       ROS_INFO("Waiting for the move_base action server to come up");
     }
-  move_base_msgs::MoveBaseGoal goal;
+    move_base_msgs::MoveBaseGoal goal;
 
+    //we'll send a goal to the robot to move 1 meter forward
+    goal.target_pose.header.frame_id = "base_link";
+    goal.target_pose.header.stamp = ros::Time::now();
 
+    goal.target_pose.pose.position.x = posX;
+    goal.target_pose.pose.position.y = posY;
+
+    if (oriantation == "Right")
+    {
+      goal.target_pose.pose.orientation.w = -sqrt(0.5);
+      goal.target_pose.pose.orientation.z = sqrt(0.5);
+    }
+    else if (oriantation == "Left")
+    {
+      goal.target_pose.pose.orientation.w = sqrt(0.5);
+      goal.target_pose.pose.orientation.z = -sqrt(0.5);
+    }
+    else if (oriantation == "Backwards")
+    {
+      goal.target_pose.pose.orientation.w = 0;
+      goal.target_pose.pose.orientation.z = 1;
+    }
+    else
+    {
+      goal.target_pose.pose.orientation.w = 1;
+      goal.target_pose.pose.orientation.z = 0;
+    }
+    ROS_INFO("Sending goal");
+    ac.sendGoal(goal);
+
+    ac.waitForResult();
+
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+      ROS_INFO("Sucess, the base moved to the goal");
+    else
+      ROS_INFO("The base failed to move to the goal");
+  }
+
+  void moveToMap(double posX, double posY, const char *oriantation)
+  {
+    MoveBaseClient ac("move_base", true);
+
+    //wait for the action server to come up
+    while (!ac.waitForServer(ros::Duration(5.0)))
+    {
+      ROS_INFO("Waiting for the move_base action server to come up");
+    }
+    move_base_msgs::MoveBaseGoal goal;
 
     //we'll send a goal to the robot to move 1 meter forward
     goal.target_pose.header.frame_id = "map";
@@ -63,40 +111,42 @@ class MovingToPosition{
     goal.target_pose.pose.position.x = posX;
     goal.target_pose.pose.position.y = posY;
 
-  //orientation based on Quaternions http://wiki.ogre3d.org/Quaternion+and+Rotation+Primer
-  if(oriantation == "Right"){
-    goal.target_pose.pose.orientation.w = -sqrt(0.5);
-    goal.target_pose.pose.orientation.z = sqrt(0.5);
-  }else if(oriantation == "Left"){
-    goal.target_pose.pose.orientation.w = sqrt(0.5);
-    goal.target_pose.pose.orientation.z = -sqrt(0.5);
-  }
-  else if(oriantation == "Backwards"){
-    goal.target_pose.pose.orientation.w = 0;
-    goal.target_pose.pose.orientation.z = 1;	
-  }else{
-    goal.target_pose.pose.orientation.w = 1;
-    goal.target_pose.pose.orientation.z = 0;	
-  }
+    //orientation based on Quaternions http://wiki.ogre3d.org/Quaternion+and+Rotation+Primer
+    if (oriantation == "Right")
+    {
+      goal.target_pose.pose.orientation.w = -sqrt(0.5);
+      goal.target_pose.pose.orientation.z = sqrt(0.5);
+    }
+    else if (oriantation == "Left")
+    {
+      goal.target_pose.pose.orientation.w = sqrt(0.5);
+      goal.target_pose.pose.orientation.z = -sqrt(0.5);
+    }
+    else if (oriantation == "Backwards")
+    {
+      goal.target_pose.pose.orientation.w = 0;
+      goal.target_pose.pose.orientation.z = 1;
+    }
+    else
+    {
+      goal.target_pose.pose.orientation.w = 1;
+      goal.target_pose.pose.orientation.z = 0;
+    }
     ROS_INFO("Sending goal");
     ac.sendGoal(goal);
 
     ac.waitForResult();
 
-    if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+    if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
       ROS_INFO("Sucess, the base moved to the goal");
     else
       ROS_INFO("The base failed to move to the goal");
-
   }
 };
 
-//
-
-
 //Actionlib
-typedef actionlib::SimpleActionClient <kobuki_msgs::AutoDockingAction> dockingClient;
-typedef actionlib::SimpleActionClient <move_base_msgs::MoveBaseAction> moveBaseClient;
+typedef actionlib::SimpleActionClient<kobuki_msgs::AutoDockingAction> dockingClient;
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> moveBaseClient;
 kobuki_msgs::AutoDockingGoal dockingGoal;
 move_base_msgs::MoveBaseGoal homeGoal;
 
@@ -115,19 +165,19 @@ geometry_msgs::Pose2D startPose;
 nav_msgs::Odometry odomPose;
 
 //Declaration of global variables
-geometry_msgs::Pose2D currentPose;//Constantly updated position of robot
-geometry_msgs::Pose2D savedPose;  //Saved position before heading home
+geometry_msgs::Pose2D currentPose; //Constantly updated position of robot
+geometry_msgs::Pose2D savedPose;   //Saved position before heading home
 
 //Initialisation of global variables
 bool fullyCharged = false;
-//int const kobuki_max_charge_voltage = 163;//Voltage from base battery at full charge (measured in 0.1V) 
+//int const kobuki_max_charge_voltage = 163;//Voltage from base battery at full charge (measured in 0.1V)
 
 //Callback function saves x and y coordinates from subscriber
-void callbackStartPose(const geometry_msgs::Pose2D startPose) 
+void callbackStartPose(const geometry_msgs::Pose2D startPose)
 {
   homeGoal.target_pose.pose.position.x = startPose.x;
   homeGoal.target_pose.pose.position.y = startPose.y;
-  startPoseSub.shutdown();//Subscriber only needs to update home once (tested this works)
+  startPoseSub.shutdown(); //Subscriber only needs to update home once (tested this works)
 }
 
 //Callback function saves x and y coordinates from subscriber
@@ -153,8 +203,8 @@ void headHomeToCharge()
   currentPoseSaver(); //Save current location before returning
   std::cout << "Currently at x = " << savedPose.x << ", y = " << savedPose.y << std::endl;
 
-  MovingToPosition moveInMap;
-  moveInMap.moveToMap(homeGoal.target_pose.pose.position.x, homeGoal.target_pose.pose.position.y, "Forwards");
+  MovingToPosition moveClass;
+  moveClass.moveToMap(homeGoal.target_pose.pose.position.x, homeGoal.target_pose.pose.position.y, "Forwards");
   /*
   //Drive towards starting position (following a safe route)
   moveBaseClient client1("move_base");//Starts client as move_base
@@ -175,10 +225,10 @@ void headHomeToCharge()
   //Docking procedure
   dockingClient client2("dock_drive_action", true); //Starts client, needs to be called "dock_drive_action" to work (true -> don't need ros::spin())
   client2.waitForServer();                          //Wait for feedback from the Action server
-  
+
   //kobuki_msgs::AutoDockingGoal dockingGoal;         //Sets docking as the goal
-  client2.sendGoal(dockingGoal);                    //Sends new goal to nodelet managing the docking procedure (check /opt/ros/kinetic/share/kobuki_auto_docking/launch/minimal.launch for additions to launch file)
-  client2.waitForResult();                          //ros::Duration(5.0) for maximum wait time?
+  client2.sendGoal(dockingGoal); //Sends new goal to nodelet managing the docking procedure (check /opt/ros/kinetic/share/kobuki_auto_docking/launch/minimal.launch for additions to launch file)
+  client2.waitForResult();       //ros::Duration(5.0) for maximum wait time?
 
   if (client2.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
   {
@@ -198,9 +248,9 @@ void resumeDemining()
   //Resume demining task
 }
 
-void callbackKobukiBatState(const kobuki_msgs::PowerSystemEvent kobBatState)//removed & before kobBatState
+void callbackKobukiBatState(const kobuki_msgs::PowerSystemEvent kobBatState) //removed & before kobBatState
 {
-  switch(kobBatState.event)
+  switch (kobBatState.event)
   {
   case 0: //Unplugged from charger
     if (!fullyCharged)
@@ -227,7 +277,7 @@ void callbackKobukiBatState(const kobuki_msgs::PowerSystemEvent kobBatState)//re
 
   case 4: //Base is low on battery (15%)
     std::cout << "Base is low on battery. Pausing operation and heading to dock" << std::endl;
-    
+
     headHomeToCharge(); //Send robot home to recharge base
     break;
 
@@ -235,7 +285,7 @@ void callbackKobukiBatState(const kobuki_msgs::PowerSystemEvent kobBatState)//re
     std::cout << "Base battery level is critical!" << std::endl;
     break;
 
-  default://Error! this should be unreachable
+  default: //Error! this should be unreachable
     std::cout << "Something when wrong with /mobile_base/events/power_system, it sent wrong data" << std::endl;
     break;
   }
@@ -246,7 +296,7 @@ void callbackLaptopBat(const sensor_msgs::BatteryState laptopBatLevel)
   std::cout << "Laptop battery is currently at " << laptopBatLevel.percentage << "%" << std::endl;
 
   //Testet og virker
-  if (laptopBatLevel.percentage <= 15 && laptopBatLevel.power_supply_status != 1)//When laptop reaches 15% remaining power it the robot shall return home
+  if (laptopBatLevel.percentage <= 15 && laptopBatLevel.power_supply_status != 1) //When laptop reaches 15% remaining power it the robot shall return home
   {
     std::cout << "Laptop is low on battery. Pausing operation and heading to dock" << std::endl;
 
@@ -272,11 +322,11 @@ int main(int argc, char *argv[])
   startPoseSub = n.subscribe("/start_position", 1, callbackStartPose);
 
   //Subscribes to the current position of the robot
-  currentPoseSub = n.subscribe ("/odom", 1, callbackCurrentPose);
+  currentPoseSub = n.subscribe("/odom", 1, callbackCurrentPose);
 
   //Subscribes to the laptop battery
   laptopBatlevelSub = n.subscribe("/laptop_charge", 1, callbackLaptopBat);
-  
+
   //More precise battery function with percentage option
   //kobukiBatlevelSub = n.subscribe("/mobile_base/sensors/core", 10, callbackKobukiBat);
 
@@ -286,27 +336,29 @@ int main(int argc, char *argv[])
   //Test without low battery
   //ros::spinOnce();
   //headHomeToCharge();
-  
-  /*
-  while(ros::ok())
+
+  int lastInput;
+  while (ros::ok())
   {
-    currentPoseSaver(); //Save current location before returning
-    std::cout << "Currently at x = " << savedPose.x << ", y = " << savedPose.y << std::endl;
-    std::cin.get();
-    //currentPoseSaver(); //Save current location before returning
-    //std::cout << "Currently at x = " << savedPose.x << ", y = " << savedPose.y << std::endl;
-    
-    //std::cout << "Home at x = " << homeGoal.target_pose.pose.position.x << ", y = " << homeGoal.target_pose.pose.position.y << std::endl;
-
-
-    ros::spinOnce();//Updates all subscribed and published information
+    ros::spinOnce(); //Updates all subscribed and published information
+    std::cout << "input a number from 1 to 3: " << std::endl;
+    std::cin >> lastInput;
+    switch (lastInput)
+    {
+    case 1:
+      std::cout << "Start position at x = " << startPose.x << ", y = " << startPose.y << std::endl;
+      break;
+    case 2:
+      headHomeToCharge();
+      break;
+    case 3:
+      std::cout << "Ending program" << std::endl;
+      return 0;
+    default:
+      std::cout << "Other input recieved" << std::endl;
+    }
   }
-  */
 
-  currentPoseSaver(); //Save current location before returning
-  std::cout << "Currently at x = " << savedPose.x << ", y = " << savedPose.y << std::endl;
-  std::cin.get();
-
-  ros::spin();
+  //ros::spin();
   return 0;
 }
--- demining/src/coordinatePublisher.cpp
+++ demining/src/coordinatePublisher.cpp
@@ -1,7 +1,42 @@
 #include <ros/ros.h>
 #include <iostream>                       
 #include <geometry_msgs/Pose2D.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/simple_client_goal_state.h>
 #include <nav_msgs/Odometry.h>
+
+//Following inclusions used for MovingToPosition class
+#include <tf/transform_listener.h>
+#include <geometry_msgs/PointStamped.h>
+
+class MovingToPosition
+{
+private:
+  tf::TransformListener listener;
+
+  typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+public:
+  double getPosition(double mapPose[])
+  {
+    ros::spinOnce();
+    geometry_msgs::PoseStamped pBase, pMap;
+    pBase.header.frame_id = "base_link";
+    pBase.pose.position.x = 0.0;
+    pBase.pose.position.y = 0.0;
+    pBase.pose.orientation = tf::createQuaternionMsgFromYaw(0.0);
+    ros::Time current_transform = ros::Time::now();
+    ros::Duration(0.2).sleep();
+    listener.getLatestCommonTime(pBase.header.frame_id, "map", current_transform, NULL);
+    ros::Duration(0.2).sleep();
+    pBase.header.stamp = current_transform;
+    listener.transformPose("map", pBase, pMap);
+    //printf("robot pose: (%.2f, %.2f)\n", pMap.pose.position.x, pMap.pose.position.y);
+    mapPose[0] = pMap.pose.position.x;
+    mapPose[1] = pMap.pose.position.y;
+  }
+};
 
 ros::Publisher startPosePub;
 ros::Subscriber startPoseSub;
@@ -22,9 +57,15 @@ int main(int argc, char *argv[])
   ros::NodeHandle n;
 
   startPosePub = n.advertise <geometry_msgs::Pose2D> ("/start_position", 1, true);
-  startPoseSub = n.subscribe ("/odom", 1, startPoseAssign);
+  //startPoseSub = n.subscribe ("/odom", 1, startPoseAssign);
+
+  double startPoseArr[2];
+  MovingToPosition moveClass;
+  moveClass.getPosition(startPoseArr);
+  startPose.x = startPoseArr[0];
+  startPose.y = startPoseArr[1];
+  startPosePub.publish(startPose);
 
   ros::spin();
-
   return 0;
 }
