ba2f20d62fcbb4f13568d3a45db8a3e54564d136
SimonHaaningAAU <san20@student.aau.dk>
2020-11-27 15:56:11 +0100

    coordinate publisher updated to match new topic

--- demining/src/coordinatePublisher.cpp
+++ demining/src/coordinatePublisher.cpp
@@ -1,10 +1,9 @@
 #include <ros/ros.h>
 #include <iostream>                       
-#include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/Pose.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/simple_client_goal_state.h>
-#include <nav_msgs/Odometry.h>
 
 //Following inclusions used for MovingToPosition class
 #include <tf/transform_listener.h>
@@ -39,31 +38,21 @@ public:
 };
 
 ros::Publisher startPosePub;
-ros::Subscriber startPoseSub;
 
-geometry_msgs::Pose2D startPose;
-
-void startPoseAssign(const nav_msgs::Odometry odomPose)
-{
-  startPose.x = odomPose.pose.pose.position.x;
-  startPose.y = odomPose.pose.pose.position.y;
-  startPoseSub.shutdown();
-  startPosePub.publish(startPose);
-}
+geometry_msgs::Pose startPose;
 
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "coordinatePublisher");
   ros::NodeHandle n;
 
-  startPosePub = n.advertise <geometry_msgs::Pose2D> ("/start_position", 1, true);
-  //startPoseSub = n.subscribe ("/odom", 1, startPoseAssign);
+  startPosePub = n.advertise <geometry_msgs::Pose> ("start_position", 1, true);
 
   double startPoseArr[2];
   MovingToPosition moveClass;
   moveClass.getPosition(startPoseArr);
-  startPose.x = startPoseArr[0];
-  startPose.y = startPoseArr[1];
+  startPose.position.x = startPoseArr[0];
+  startPose.position.y = startPoseArr[1];
   startPosePub.publish(startPose);
 
   ros::spin();
